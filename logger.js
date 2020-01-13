51b8a439c1f32df163afa08830f3eba47b21a809
Milan <milan.dhar@gmail.com>
2020-01-12 18:30:35 -0800

    finished tutorial

---
+++ logger.js
@@ -0,0 +1,25 @@
+const EventEmitter = require('events')
+const uuid = require('uuid')
+const Logger = require('./logger'); 
+
+
+// console.log(uuid.v4())
+
+class Logger extends EventEmitter {
+    log(msg) {
+        // Call event
+        this.emit('message', { id: uuid.v4(), msg })
+    }
+}
+
+// module.exports = Logger; 
+
+const logger = new Logger(); 
+
+logger.on('message', (data) => console.log('Called Listener', data)); 
+
+logger.log('Hello World')
+logger.log('Hi')
+logger.log('Hello')
+
+
